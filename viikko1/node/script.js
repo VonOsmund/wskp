45e959add04797a9a3bfd801d4a05ac78be4eb3f
Von Osmund <ossisorjonen@gmail.com>
2019-10-25 11:55:02 +0300

    viikko 1

---
+++ viikko1/node/script.js
@@ -0,0 +1,5 @@
+'use strict';
+const _ = require('lodash');
+console.log('2:', 'Oispa kaljaa');
+var a = _.camelCase('Foo Bar');
+console.log('5:',a);
---
+++ viikko1/server-two/script.js
@@ -0,0 +1,22 @@
+'use strict'
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'pug');
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+});
+
+app.use(express.static('public'));
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+        'name': 'Frank',
+        'age': 6,
+        'weight': 5,
+    };
+res.json(cat);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
